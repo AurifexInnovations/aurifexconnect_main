1eced7ada83823d681b7681f50e49c0c8e1e9fa2
yagneshchudasasma23@gmail.com <Yagnesh23@>
2025-10-10 19:15:46 +0530

    remove feign related imports and updated poms.

--- ERP/src/main/java/com/erp/ERPApplication.java
+++ ERP/src/main/java/com/erp/ERPApplication.java
@@ -2,7 +2,6 @@ package com.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableRetry
-@EnableFeignClients(basePackages = "com.erp.FeignClient.Otp")
 public class ERPApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ERPApplication.class, args);
