0c018a767b015fb918814b3b0b2d98d1a5537989
Aryan Raval <aryanraval299@gmail.com>
2025-11-01 00:50:03 +0530

    Company Details Bug Fixing (#78)
    Compnay DEtails Bug Fixing

--- ERP/src/main/java/com/erp/Dto/Request/CompanyDetailsRequestDto.java
+++ ERP/src/main/java/com/erp/Dto/Request/CompanyDetailsRequestDto.java
@@ -2,15 +2,24 @@ package com.erp.Dto.Request;
 
 import java.util.List;
 
-public record CompanyDetailsRequestDto(Long id,
-                                       String name,
-                                       String address,
-                                       String description,
-                                       String gstNumber,
-                                       String panNumber,
-                                       String industryType,
-                                       String reviewStatus,
-                                       String reviewComment,
-                                       String reviewedBy,
-                                       List<DocumentDetailsRequestDto> documentDetails) {
-}
+public record CompanyDetailsRequestDto(
+        Long id,
+        String name,
+        Integer officeNo,
+        String addressLine1,
+        String addressLine2,
+        String description,
+        String gstNumber,
+        String panNumber,
+        String industryType,
+        String reviewStatus,
+        String reviewComment,
+        String contactPersonName,
+        String contactPersonEmail,
+        String contactPersonPhone,
+        String pincode,
+        String city,
+        String state,
+        String reviewedBy,
+        List<DocumentDetailsRequestDto> documentDetails
+) {}
