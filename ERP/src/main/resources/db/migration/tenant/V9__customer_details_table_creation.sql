5879379d5de78545021df57ec82e155b4ebe876c
Aryan Raval <aryanraval299@gmail.com>
2025-10-31 23:07:26 +0530

    Bug Fixing (#77)

--- ERP/src/main/resources/db/migration/tenant/V9__customer_details_table_creation.sql
+++ ERP/src/main/resources/db/migration/tenant/V9__customer_details_table_creation.sql
@@ -11,6 +11,6 @@ CREATE TABLE IF NOT EXISTS customer_details (
     created_by BIGINT,
     created_date TIMESTAMP,
     last_modified_by BIGINT,
-    last_modified_date TIMESTAMP
+    last_modified_date TIMESTAMP,
     FOREIGN KEY (lead_id) REFERENCES lead_details(id)
 );
