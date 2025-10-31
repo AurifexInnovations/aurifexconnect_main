c5d15d628af5df9b5c15396e38aaeb7d992fa9c0
deva964 <aryanraval299@gmail.com>
2025-11-01 00:47:05 +0530

    Company Details Bug Fixing

--- ERP/src/main/java/com/erp/Model/CompanyDetails.java
+++ ERP/src/main/java/com/erp/Model/CompanyDetails.java
@@ -1,5 +1,7 @@
 package com.erp.Model;
 
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import com.erp.Enum.IndustryType;
 import com.erp.Enum.ReviewStatus;
@@ -8,35 +10,42 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
+@Table(name = "company_details")
 @Getter
 @Setter
-@Table(name = "company_details")
+@NoArgsConstructor
+@AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class CompanyDetails extends BaseEntity {
-
     @Column(name = "name")
     private String name;
 
-    @Column(name = "address")
-    private String address;
+    @Column(name = "office_no")
+    private Integer officeNo;
+
+    @Column(name = "address_line1", length = 500)
+    private String addressLine1;
+
+    @Column(name = "address_line2", length = 500)
+    private String addressLine2;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "gst_number", length = 50)
+    private String gstNumber;
+
+    @Column(name = "pan_number", length = 50)
+    private String panNumber;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "industry_type")
     private IndustryType industryType;
-
-    @Column(name = "description", columnDefinition = "TEXT")
-    private String description;
-
-    @Column(name = "gst_number")
-    private String gstNumber;
-
-    @Column(name = "pan_number")
-    private String panNumber;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "review_status")
@@ -45,6 +54,24 @@ public class CompanyDetails extends BaseEntity {
     @Column(name = "review_comment", columnDefinition = "TEXT")
     private String reviewComment;
 
+    @Column(name = "contact_person_name")
+    private String contactPersonName;
+
+    @Column(name = "contact_person_email")
+    private String contactPersonEmail;
+
+    @Column(name = "contact_person_phone")
+    private String contactPersonPhone;
+
+    @Column(length = 10)
+    private String pincode;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
     @Column(name = "reviewed_by")
     private String reviewedBy;
 
@@ -52,7 +79,4 @@ public class CompanyDetails extends BaseEntity {
             cascade = CascadeType.ALL,
             orphanRemoval = true)
     private List<DocumentDetails> documentDetails;
-
 }
-
-
