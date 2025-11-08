41eef0be1281056eb9a9146db7ed81a95a652fce
Sumit-Rajak <sumitbhairajak@gmail.com>
2025-11-08 11:38:33 +0530

    header added at and rt

--- ERP/src/main/java/com/erp/Security/Config/SecurityConfig.java
+++ ERP/src/main/java/com/erp/Security/Config/SecurityConfig.java
@@ -37,6 +37,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -139,6 +141,19 @@ public class SecurityConfig {
                 .build();
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*")
+                        .allowCredentials(true)
+                        .exposedHeaders("rt", "at");
+            }
+        };
+    }
     // ------------------ Refresh Token Filter Chain ------------------
     @Bean
     @Order(2)
