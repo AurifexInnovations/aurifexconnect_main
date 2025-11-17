d3277670e44b486ebe9b43926b4cbe6c5065530c
Veeratsolanki27 <27veerat@gmail.com>
2025-11-17 23:16:47 +0530

    Made some changes

--- ERP/src/main/resources/db/migration/tenant/V18__company_details_connectin.sql
+++ ERP/src/main/resources/db/migration/tenant/V18__company_details_connectin.sql
@@ -1,5 +1,5 @@
 
 -- ADD COLUMN
---ALTER TABLE company_details
---ADD COLUMN company_email VARCHAR(60),
---ADD CONSTRAINT uq_company_details_email UNIQUE (company_email);
+ALTER TABLE company_details
+ADD COLUMN company_email VARCHAR(60),
+ADD CONSTRAINT uq_company_details_email UNIQUE (company_email);
