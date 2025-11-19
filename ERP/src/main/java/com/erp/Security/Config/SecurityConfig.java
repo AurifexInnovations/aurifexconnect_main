88a2ce9febd80006f4ecdd5fc0108330abe6a0c2
deva964 <aryanraval299@gmail.com>
2025-11-19 10:04:05 +0530

    Added Domain URL in Cors Config

--- ERP/src/main/java/com/erp/Security/Config/SecurityConfig.java
+++ ERP/src/main/java/com/erp/Security/Config/SecurityConfig.java
@@ -192,7 +192,7 @@ public class SecurityConfig {
     {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:5174","https://magenta-cranachan-beb97b.netlify.app"));
+        config.setAllowedOrigins(List.of("http://localhost:3000","https://seravionerp.netlify.app","http://localhost:5174","https://magenta-cranachan-beb97b.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
