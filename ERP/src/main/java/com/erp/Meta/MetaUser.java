a7d76f3dbad496e84ceabc9bc4ead3c71f2be00f
Aryan Raval <aryanraval299@gmail.com>
2025-11-26 18:49:23 +0530

    User Login And Admin Creation (Priority High) (#112)
    * Admin Creation and User Login
    
    * Final User login and Admin Creation

--- ERP/src/main/java/com/erp/Meta/MetaUser.java
+++ ERP/src/main/java/com/erp/Meta/MetaUser.java
@@ -1,32 +1,24 @@
-//package com.erp.Meta;
-//
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//import org.springframework.data.annotation.CreatedDate;
-//import org.springframework.data.annotation.LastModifiedDate;
-//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-//import java.time.LocalDateTime;
-//
-//@Entity
-//@Getter
-//@Setter
-//@EntityListeners(AuditingEntityListener.class)
-//public class MetaUser {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private long metaUserId;
-//
-//    @Column(name = "user_email", nullable = false, unique = true)
-//    private String userEmail;
-//
-//    @Column(name = "schema_name", nullable = false, unique = true)
-//    private String schemaName;
-//
-//    @CreatedDate
-//    private LocalDateTime createdAt;
-//
-//    @LastModifiedDate
-//    private LocalDateTime lastModifiedAt;
-//}
+package com.erp.Meta;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "meta_user", schema = "public")
+@EntityListeners(AuditingEntityListener.class)
+public class MetaUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_email", nullable = false, unique = true)
+    private String userEmail;
+
+    @Column(name = "schema_name", nullable = false, unique = true)
+    private String schemaName;
+}
--- ERP/src/main/java/com/erp/Meta/MetaUserRepository.java
+++ ERP/src/main/java/com/erp/Meta/MetaUserRepository.java
@@ -1,15 +1,18 @@
 package com.erp.Meta;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface MetaUserRepository {
+public interface MetaUserRepository extends JpaRepository<MetaUser, Long> {
 
-    Optional<MetaAdmin> findByUserEmail(String email);
+    Optional<MetaUser> findByUserEmail(String email);
 
     boolean existsByUserEmail(String email);
+
+    boolean existsBySchemaName(String schemaName);
 
     @Query("SELECT m.schemaName FROM MetaUser m WHERE m.userEmail = :email")
     Optional<String> findSchemaNameByUserEmail(@Param("email") String email);
